ea2eb16485569edb0f7d931d4216a7cbda8bf9dd
cybtetuanui92 <mateau.tetuanui@gmail.com>
2024-04-29 09:02:23 +0200

    commitbeforeimportcity

---
+++ src/Entity/Ads.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AdsRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AdsRepository::class)]
+class Ads
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $datePosted = null;
+
+    #[ORM\ManyToOne]
+    private ?Categories $categoryId = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getDatePosted(): ?\DateTimeInterface
+    {
+        return $this->datePosted;
+    }
+
+    public function setDatePosted(\DateTimeInterface $datePosted): static
+    {
+        $this->datePosted = $datePosted;
+
+        return $this;
+    }
+
+    public function getCategoryId(): ?Categories
+    {
+        return $this->categoryId;
+    }
+
+    public function setCategoryId(?Categories $categoryId): static
+    {
+        $this->categoryId = $categoryId;
+
+        return $this;
+    }
+}
